592b4f38a328540a1e31c8d1c361298001568cd0
Peter Poole <peter.j.poole@gmail.com>
2024-07-17 16:40:57 -0400

    add unsigned and percentile helper functions

--- lib/server/parsers/parsers.ts
+++ lib/server/parsers/parsers.ts
@@ -8,11 +8,32 @@ export function keyToInteger<T>(key: keyof T): ParserFunction {
   }
 }
 
+export function keyToUnsigned<T>(key: keyof T): ParserFunction {
+  return function (value: ParserValues) {
+    const current = this.current
+    const int = valueAsInteger(value)
+    if (int < 0) throw new Error('invalid value for unsigned int')
+    current[key] = int
+  }
+}
+
 export function keyToString<T>(key: keyof T): ParserFunction {
   return function (value: ParserValues) {
     const current = this.current
     if (!current[key]) current[key] = ''
     current[key] += value
+  }
+}
+
+export function keyToPercentile<T>(key: keyof T): ParserFunction {
+  return function (value: ParserValues) {
+    const current = this.current
+    let percentile = valueAsInteger(value)
+    if (percentile < 1 || percentile > 100) throw new Error('invalid percentile value', { cause: { value } })
+
+    // TODO: Figure out the logic here; could have some rounding issues
+    //       this is original behavior, though
+    current[key] = 100 / percentile
   }
 }
 
