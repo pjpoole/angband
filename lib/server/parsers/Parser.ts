a9030243fdfaba0171f52317d3ee33e95fc64f53
Peter Poole <peter.j.poole@gmail.com>
2024-07-17 14:25:23 -0400

    basic implementation of data file parser

---
+++ lib/server/parsers/Parser.ts
@@ -0,0 +1,50 @@
+import { GameObject } from '../../common/GameObject'
+
+export type ParserValues = string
+
+type ParserFunction = (values: ParserValues) => void
+
+export abstract class Parser<T extends GameObject> {
+  private _error: Error
+  private _handlers: Map<keyof T, ParserFunction>
+  private _objects: Partial<T>[]
+  private _current: Partial<T> | null = null
+
+  parse(key: keyof T, value: ParserValues): void {
+    const handler = this._handlers.get(key)
+    if (!handler) {
+      throw new Error('no handler for key', { cause: { key } })
+    }
+    handler(value)
+  }
+
+  abstract finalize(): void
+
+  register(key: keyof T, handler: ParserFunction): void {
+    this._handlers.set(key, handler)
+  }
+
+  get objects(): T[] {
+    return this._objects as T[]
+  }
+
+  hasCurrent(): boolean {
+    return this._current != null
+  }
+
+  newCurrent(): Partial<T> {
+    this._current = {}
+    return this._current
+  }
+
+  get current(): Partial<T> {
+    return this._current
+  }
+
+  finalizeCurrent(): void {
+    this._objects.push(this._current)
+  }
+
+  set error(err: Error) { this._error = err }
+  get error(): Error { return this._error }
+}
