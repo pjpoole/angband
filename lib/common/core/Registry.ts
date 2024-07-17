6f3c222dbe4bc3419a83a6a94fd5c59ff792d7d9
Peter Poole <peter.j.poole@gmail.com>
2024-07-17 14:27:34 -0400

    basic registry class

---
+++ lib/common/core/Registry.ts
@@ -0,0 +1,22 @@
+import { GameObject } from '../GameObject'
+
+export class Registry<T> {
+  private data: Map<string, T> = new Map()
+
+  constructor(private readonly ctor: new (params: GameObject) => T) {
+    this.ctor = ctor
+  }
+
+  get(key: string): T {
+    return this.data.get(key)
+  }
+
+  build(key: string, data: GameObject): void {
+    const obj = new this.ctor(data)
+    this.add(key, obj)
+  }
+
+  add(key: string, obj: T): void {
+    this.data.set(key, obj)
+  }
+}
