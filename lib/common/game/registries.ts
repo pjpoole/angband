f4a7e9c5c9a4256503ca217a0f4203d23d133e80
Peter Poole <peter.j.poole@gmail.com>
2024-08-19 20:19:47 -0400

    imports in order by path; exports in order by name

--- lib/common/game/registries.ts
+++ lib/common/game/registries.ts
@@ -1,19 +1,21 @@
 import { NameRegistry, IdRegistry } from '../core/Registry'
+
 import { Blow } from '../monsters/blows'
 import { MonsterBase } from '../monsters/monsterBase'
-import { Projection } from '../spells/Projection'
+import { Curse } from '../objects/curse'
 import { ObjectBase } from '../objects/objectBase'
 // import { PlayerProperty } from '../player/properties'
+import { Projection } from '../spells/Projection'
 import { DungeonProfile } from '../world/dungeonProfiles'
 import { Feature } from '../world/features'
 import { Level } from '../world/level'
 
-
-export const DungeonProfileRegistry = new IdRegistry(DungeonProfile)
-export const LevelRegistry = new IdRegistry(Level)
-export const ObjectBaseRegistry = new NameRegistry(ObjectBase)
-export const ProjectionRegistry = new IdRegistry(Projection)
-export const FeatureRegistry = new IdRegistry(Feature)
-// export const PlayerPropertyRegistry = new IdRegistry(PlayerProperty)
 export const BlowRegistry = new NameRegistry(Blow)
+export const CurseRegistry = new NameRegistry(Curse)
+export const DungeonProfileRegistry = new IdRegistry(DungeonProfile)
+export const FeatureRegistry = new IdRegistry(Feature)
+export const LevelRegistry = new IdRegistry(Level)
 export const MonsterBaseRegistry = new NameRegistry(MonsterBase)
+export const ObjectBaseRegistry = new NameRegistry(ObjectBase)
+// export const PlayerPropertyRegistry = new IdRegistry(PlayerProperty)
+export const ProjectionRegistry = new IdRegistry(Projection)
