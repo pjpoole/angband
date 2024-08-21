ebb17a2bc14f5567ff04df6b4029ff9a410dba9d
Peter Poole <peter.j.poole@gmail.com>
2024-08-21 13:26:09 -0400

    forward parseEffect to asEffect

--- lib/server/parsers/helpers.ts
+++ lib/server/parsers/helpers.ts
@@ -10,16 +10,19 @@ import {
 } from '../../common/utilities/zod'
 import { EF } from '../../common/spells/effects'
 import { EX } from '../../common/spells/expressions'
+import { asEffect } from '../../common/utilities/parseGameObjects'
 
 export function parseCombat(values: ParserValues): CombatJSON {
   const [toHit, toDamage, toAC] = asTokens(values, 3)
   return { toHit, toDamage, toAC }
 }
 
-// TODO: Modify me once we narrow the types of the other tokens
 function effectsAreEqual(eff1: zEffectJSON, eff2: zEffectJSON): boolean {
   return (
-    eff1.effect === eff2.effect
+    eff1.effect === eff2.effect &&   // EF
+    eff1.subType === eff2.subType && // string | undefined
+    eff1.radius === eff2.radius &&   // number | undefined
+    eff1.other === eff2.other        // number | undefined
   )
 }
 
@@ -39,11 +42,7 @@ export function parseEffects(values: ParserValues, current: zEffectJSON[] = []):
 }
 
 export function parseEffect(values: ParserValues): zEffectJSON {
-  const tokens = asTokens(values, 1, 4)
-  // TODO: figure out types of other 3 tokens
-  return {
-    effect: asEnum(tokens[0], EF)
-  }
+  return asEffect(values)
 }
 
 export function parseExpression(values: ParserValues): zExpressionJSON {
