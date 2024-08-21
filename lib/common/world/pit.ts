4e991bce2620286f3960f4586c4cdb1dae5d2f83
Peter Poole <peter.j.poole@gmail.com>
2024-08-21 19:17:06 -0400

    add pit and parser

---
+++ lib/common/world/pit.ts
@@ -0,0 +1,113 @@
+import { z } from 'zod'
+import { SerializableBase } from '../core/serializable'
+import { z_enumValueParser } from '../utilities/zod'
+
+import { C } from '../utilities/colors'
+import { RF } from '../monsters/flags'
+import { MonsterBase, MonsterBaseRegistry } from '../monsters/monsterBase'
+import { RSF } from '../monsters/spells'
+import { NameRegistry } from '../core/Registry'
+import { enumValueSetToArray } from '../utilities/enum'
+import { Monster, MonsterRegistry } from '../monsters/monster'
+
+const monsterBaseFinder = z.string().transform((str, ctx) => {
+  const monsterBase = MonsterBaseRegistry.get(str)
+  if (monsterBase != null) return monsterBase
+
+  ctx.addIssue({
+    code: z.ZodIssueCode.custom,
+    message: 'invalid monster base type'
+  })
+  return z.NEVER
+})
+
+const monsterFinder = z.string().transform((str, ctx) => {
+  const monster = MonsterRegistry.get(str)
+  if (monster != null) return monster
+
+  ctx.addIssue({
+    code: z.ZodIssueCode.custom,
+    message: 'invalid monster type'
+  })
+  return z.NEVER
+})
+
+// TODO: Spells, flags, monsters, monster bases
+export const PitSchema = z.object({
+  name: z.string(),
+  room: z.number().int().gte(1).lte(3), // TODO: where do these come from?
+  rarity: z.number(),
+  averageLevel: z.number(),
+  objectRarity: z.number().optional(),
+  colors: z.array(z.nativeEnum(C)).optional(),
+  monsterBases: z.array(monsterBaseFinder).optional(),
+  monstersBanned: z.array(monsterFinder).optional(),
+  flagsRequired: z.array(z_enumValueParser(RF)).optional(),
+  flagsBanned: z.array(z_enumValueParser(RF)).optional(),
+  innateFrequency: z.number().optional(),
+  spellsRequired: z.array(z_enumValueParser(RSF)).optional(),
+  spellsBanned: z.array(z_enumValueParser(RSF)).optional(),
+})
+
+export type PitJSON = z.input<typeof PitSchema>
+export type PitParams = z.output<typeof PitSchema>
+
+export class Pit extends SerializableBase {
+  static readonly schema = PitSchema
+
+  readonly name: string
+  readonly room: number
+  readonly rarity: number
+  readonly averageLevel: number
+  readonly objectRarity?: number
+  readonly colors?: C[]
+  readonly monsterBases: Set<MonsterBase>
+  readonly monstersBanned: Set<Monster>
+  readonly flagsRequired: Set<RF>
+  readonly flagsBanned: Set<RF>
+  readonly innateFrequency?: number
+  readonly spellsRequired: Set<RSF>
+  readonly spellsBanned: Set<RSF>
+
+  constructor(params: PitParams) {
+    super(params)
+
+    this.name = params.name
+    this.room = params.room
+    this.rarity = params.rarity
+    this.averageLevel = params.averageLevel
+    this.objectRarity = params.objectRarity
+    this.colors = params.colors
+    this.monsterBases = new Set(params.monsterBases)
+    this.monstersBanned = new Set(params.monstersBanned)
+    this.flagsRequired = new Set(params.flagsRequired)
+    this.flagsBanned = new Set(params.flagsBanned)
+    this.innateFrequency = params.innateFrequency
+    this.spellsRequired = new Set(params.spellsRequired)
+    this.spellsBanned = new Set(params.spellsBanned)
+  }
+
+  register() {
+    PitRegistry.add(this.name, this)
+  }
+
+  toJSON(): PitJSON {
+    return {
+      name: this.name,
+      room: this.room,
+      rarity: this.rarity,
+      averageLevel: this.averageLevel,
+      objectRarity: this.objectRarity,
+      colors: this.colors,
+      monsterBases: this.monsterBases.size > 0 ? Array.from(this.monsterBases).map(monsterBase => monsterBase.name) : undefined,
+      monstersBanned: this.monstersBanned.size > 0 ? Array.from(this.monstersBanned).map(monsterbase => monsterbase.name) : undefined,
+      flagsRequired: enumValueSetToArray(this.flagsRequired, RF),
+      flagsBanned: enumValueSetToArray(this.flagsBanned, RF),
+      innateFrequency: this.innateFrequency,
+      spellsRequired: enumValueSetToArray(this.spellsRequired, RSF),
+      spellsBanned: enumValueSetToArray(this.spellsBanned, RSF),
+    }
+  }
+}
+
+export const PitRegistry = new NameRegistry(Pit)
---
+++ lib/server/parsers/PitParser.ts
@@ -0,0 +1,86 @@
+import { Parser } from './Parser'
+import { Pit, PitJSON, PitRegistry } from '../../common/world/pit'
+import { arrayUnion } from '../../common/utilities/array'
+import { colorStringToAttribute } from '../../common/utilities/colors'
+import {
+  allAsEnum,
+  asTokens,
+  ParserValues
+} from '../../common/utilities/parsers'
+
+import { RF } from '../../common/monsters/flags'
+import { MonsterBaseRegistry } from '../../common/monsters/monsterBase'
+import { MonsterRegistry } from '../../common/monsters/monster'
+import { RSF } from '../../common/monsters/spells'
+
+type PitFields = 'name' | 'room' | 'alloc' | 'obj-rarity' | 'color' | 'mon-base'
+  | 'flags-req' | 'flags-ban' | 'innate-freq' | 'spell-req' | 'spell-ban'
+  | 'mon-ban'
+
+export class PitParser extends Parser<PitFields, PitJSON> {
+  static readonly fileName = 'pit'
+  static readonly registry = PitRegistry
+
+  constructor() {
+    super()
+
+    this.register('name', this.stringRecordHeader('name'))
+    this.register('room', this.keyToInteger('room'))
+    this.register('alloc', this.handleAllocation.bind(this))
+    this.register('obj-rarity', this.keyToInteger('objectRarity'))
+    this.register('color', this.handleColor.bind(this))
+    this.register('mon-base', this.handleMonsterBases.bind(this))
+    this.register('mon-ban', this.handleMonstersBanned.bind(this))
+    this.register('flags-req', this.handleFlags.bind(this, 'flagsRequired'))
+    this.register('flags-ban', this.handleFlags.bind(this, 'flagsBanned'))
+    this.register('innate-freq', this.keyToInteger('innateFrequency'))
+    this.register('spell-req', this.handleSpells.bind(this, 'spellsRequired'))
+    this.register('spell-ban', this.handleSpells.bind(this, 'spellsBanned'))
+  }
+
+  _finalize(obj: PitJSON) {
+    Pit.fromJSON(obj).register()
+  }
+
+  handleAllocation(values: ParserValues) {
+    const current = this.current
+    const [rarity, averageLevel] = asTokens(values, 2).map(token => parseInt(token))
+    current.rarity = rarity
+    current.averageLevel = averageLevel
+  }
+
+  handleColor(values: ParserValues) {
+    const current = this.current
+    current.colors = arrayUnion(current.colors ?? [], [colorStringToAttribute(values)])
+  }
+
+  handleMonsterBases(values: ParserValues) {
+    const current = this.current
+    if (!MonsterBaseRegistry.has(values)) {
+      throw new Error('monster base not found', { cause: { key: values } })
+    }
+
+    current.monsterBases = arrayUnion(current.monsterBases ?? [], [values])
+  }
+
+  handleMonstersBanned(values: ParserValues) {
+    return
+    const current = this.current
+    if (!MonsterRegistry.has(values)) {
+      throw new Error('monster not found', { cause: { key: values } })
+    }
+
+    current.monstersBanned = arrayUnion(current.monstersBanned ?? [], [values])
+  }
+
+  handleFlags(key: 'flagsRequired' | 'flagsBanned', values: ParserValues) {
+    const current = this.current
+    current[key] = arrayUnion(current[key] ?? [], allAsEnum(values, RF))
+  }
+
+  handleSpells(key: 'spellsRequired' | 'spellsBanned', values: ParserValues) {
+    const current = this.current
+    current[key] = arrayUnion(current[key] ?? [], allAsEnum(values, RSF))
+  }
+
+}
