e4ee9a0c283eb73fe3452cd4791dbc23b31d03b3
Peter Poole <peter.j.poole@gmail.com>
2024-08-21 09:18:35 -0400

    add player flags

---
+++ lib/common/player/flags.ts
@@ -0,0 +1,21 @@
+// list-player-flags.h
+export enum PF {
+  NONE,
+  FAST_SHOT,
+  BRAVERY_30,
+  BLESS_WEAPON,
+  ZERO_FAIL,
+  BEAM,
+  CHOOSE_SPELLS,
+  KNOW_MUSHROOM,
+  KNOW_ZAPPER,
+  SEE_ORE,
+  NO_MANA,
+  CHARM,
+  UNLIGHT,
+  ROCK,
+  STEAL,
+  SHIELD_BASH,
+  EVIL,
+  COMBAT_REGEN,
+}
