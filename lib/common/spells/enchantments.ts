7626adf99f3759746043777c4e44471fe9fab405
Peter Poole <peter.j.poole@gmail.com>
2024-08-21 13:19:31 -0400

    add some structures that effects depend on

---
+++ lib/common/spells/enchantments.ts
@@ -0,0 +1,7 @@
+// effect-handler.h; these are inlined there (not an enum)
+export enum ENCH {
+  TOBOTH,
+  TOHIT,
+  TODAM,
+  TOAC
+}
---
+++ lib/common/spells/nourish.ts
@@ -0,0 +1,8 @@
+// inlined in effects.c
+// not in any particular place; ambient in the nourish code (short switches)
+export enum NOURISH {
+  INC_BY,
+  DEC_BY,
+  SET_TO,
+  INC_TO,
+}
