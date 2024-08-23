364e9c709dfdcdffae54294b1a97bf4be1f967ea
Peter Poole <peter.j.poole@gmail.com>
2024-08-23 09:38:58 -0400

    add combatmin to support ego items

--- lib/common/utilities/serializing/combat.ts
+++ lib/common/utilities/serializing/combat.ts
@@ -1,9 +1,22 @@
-import { CombatJSON, CombatParams } from '../zod/combat'
+import {
+  CombatJSON,
+  CombatMinJSON,
+  CombatMinParams,
+  CombatParams
+} from '../zod/combat'
 
 export function combatToJson(combat: CombatParams): CombatJSON {
   return {
     toHit: combat.toHit.toString(),
     toDamage: combat.toDamage.toString(),
     toAC: combat.toAC.toString(),
+  }
+}
+
+export function combatMinToJson(combat: CombatMinParams): CombatMinJSON {
+  return {
+    toHit: combat.toHit?.toString(),
+    toDamage: combat.toDamage?.toString(),
+    toAC: combat.toAC?.toString(),
   }
 }
--- lib/common/utilities/zod/combat.ts
+++ lib/common/utilities/zod/combat.ts
@@ -7,5 +7,11 @@ export const z_combat = z.object({
   toAC: z_diceExpression,
 })
 
+// min-combat from ego item permits "no lower bound" as a value; we represent it
+// here as "missing"
+export const z_combatMin = z_combat.partial()
+
 export type CombatJSON = z.input<typeof z_combat>
 export type CombatParams = z.output<typeof z_combat>
+export type CombatMinJSON = z.input<typeof z_combatMin>
+export type CombatMinParams = z.output<typeof z_combatMin>
