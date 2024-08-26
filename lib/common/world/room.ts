baab66c0c39bfcfbc99b37ad1e0614ea895f4a74
Peter Poole <peter.j.poole@gmail.com>
2024-08-26 18:16:54 -0400

    begin to flesh out room builder functions

--- lib/common/world/room.ts
+++ lib/common/world/room.ts
@@ -1,5 +1,25 @@
 // list-rooms.h
-const ROOM = [
+import { Dungeon } from './dungeon'
+import { Cave } from './cave'
+import { Room } from './dungeonProfiles'
+import { getConstants } from '../core/loading'
+
+type RoomBuilder = (
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+) => boolean
+
+interface RoomEntry {
+  name: string
+  rows: number
+  cols: number
+  fn: RoomBuilder
+}
+
+const ROOM: RoomEntry[] = [
   { name: 'staircase room', rows: 0, cols: 0, fn: buildStaircase },
   { name: 'simple room', rows: 0, cols: 0, fn: buildSimple },
   { name: 'moria room', rows: 0, cols: 0, fn: buildMoria },
@@ -21,6 +41,34 @@ const ROOM = [
   { name: 'Greater vault (new)', rows: 44, cols: 66, fn: buildGreaterNewVault },
 ]
 
+export function buildRoom(
+  dungeon: Dungeon,
+  chunk: Cave,
+  bx0: number,
+  by0: number,
+  profile: Room,
+  findsOwnSpace: boolean
+): boolean {
+  if (chunk.depth < profile.level) return false
+  if (profile.pit && dungeon.numPits >= getConstants().dungeonGeneration.pitMax) return false
+
+  let bx1 = bx0
+  let by1 = by0
+  let bx2 = bx0 + Math.ceil(profile.width / dungeon.blockWidth)
+  let by2 = by0 + Math.ceil(profile.height / dungeon.blockHeight)
+
+  const builder = findBuilder(profile.name)
+
+  if (findsOwnSpace) {
+    if (!builder(dungeon, chunk, chunk.width, chunk.height, profile.rating)) {
+      return false
+    }
+  }
+
+  // TODO: remove before flight
+  return false
+}
+
 export function isValidRoomName(name: string): boolean {
   for (const room of ROOM) {
     if (room.name === name) return true
@@ -29,22 +77,202 @@ export function isValidRoomName(name: string): boolean {
   return false
 }
 
-function buildStaircase() {}
-function buildSimple() {}
-function buildMoria() {}
-function buildLarge() {}
-function buildCrossed() {}
-function buildCircular() {}
-function buildOverlap() {}
-function buildTemplate() {}
-function buildInteresting() {}
-function buildPit() {}
-function buildNest() {}
-function buildHuge() {}
-function buildRoomOfChambers() {}
-function buildLesserVault() {}
-function buildMediumVault() {}
-function buildGreaterVault() {}
-function buildLesserNewVault() {}
-function buildMediumNewVault() {}
-function buildGreaterNewVault() {}
+function findBuilder(name: string): RoomBuilder {
+  for (const room of ROOM) {
+    if (room.name === name) {
+      return room.fn
+    }
+  }
+
+  throw new Error('invalid room name')
+}
+
+function buildStaircase(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildSimple(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildMoria(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildLarge(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildCrossed(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildCircular(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildOverlap(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildTemplate(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildInteresting(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildPit(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildNest(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildHuge(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildRoomOfChambers(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildLesserVault(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildMediumVault(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildGreaterVault(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildLesserNewVault(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildMediumNewVault(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
+
+function buildGreaterNewVault(
+  dungeon: Dungeon,
+  chunk: Cave,
+  x: number,
+  y: number,
+  rating: number,
+): boolean {
+  return false
+}
