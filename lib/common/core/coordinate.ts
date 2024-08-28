2f7276f8820429188f85a26ee530ac614a334642
Peter Poole <peter.j.poole@gmail.com>
2024-08-28 14:19:51 -0400

    type return values of c functions; add well-ordered helper

--- lib/common/core/coordinate.ts
+++ lib/common/core/coordinate.ts
@@ -3,19 +3,32 @@ export interface Coord {
   y: number
 }
 
-export function cEq(pt1: Coord, pt2: Coord) {
+export function cEq(pt1: Coord, pt2: Coord): boolean {
   return pt1.x === pt2.x && pt1.y === pt2.y
 }
 
-export function cSum(pt1: Coord, pt2: Coord) {
+export function cSum(pt1: Coord, pt2: Coord): Coord {
   return { x: pt1.x + pt2.x, y: pt1.y + pt2.y }
 }
 
-export function cOffset(pt: Coord, offset: number) {
+export function cOffset(pt: Coord, offset: number): Coord {
   return { x: pt.x + offset, y: pt.y + offset }
 }
 
-export function cProd(pt: Coord, multiplicand: number) {
+export function cToExteriorBox(pt1: Coord, pt2: Coord): [Coord, Coord] {
+  const [min, max] = cToWellOrdered(pt1, pt2)
+
+  return [cOffset(min, -1), cOffset(max, 1)]
+}
+
+export function cToWellOrdered(pt1: Coord, pt2: Coord): [Coord, Coord] {
+  return [
+    { x: Math.min(pt1.x, pt2.x), y: Math.min(pt1.y, pt2.y) },
+    { x: Math.max(pt1.x, pt2.x), y: Math.max(pt1.y, pt2.y) }
+  ]
+}
+
+export function cProd(pt: Coord, multiplicand: number): Coord {
   return { x: pt.x * multiplicand, y: pt.y * multiplicand }
 }
 
@@ -46,6 +59,6 @@ export function cToBox(center: Coord, height: number, width?: number): [Coord, C
   return [pt1, pt2]
 }
 
-export function cToRadius(pt: Coord, radius: number) {
+export function cToRadius(pt: Coord, radius: number): [Coord, Coord] {
   return cToBox(pt, radius * 2 + 1)
 }
