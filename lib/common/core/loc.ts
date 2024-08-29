84c878a4f9ff939ceba3c0eaa635e1a97118c808
Peter Poole <peter.j.poole@gmail.com>
2024-08-29 12:03:34 -0400

    clean up transitional loc functions

--- lib/common/core/loc.ts
+++ lib/common/core/loc.ts
@@ -2,44 +2,8 @@ export function loc(x: number, y: number): Loc {
   return new Loc(x, y)
 }
 
-export function locsToBox(p1: Loc, p2: Loc): Box {
-  return box(...wellOrder(p1, p2))
-}
-
 export function box(left: number, top: number, right: number, bottom: number): Box {
   return new Box(left, top, right, bottom)
-}
-
-export function wellOrdered(p1: Loc, p2: Loc): [Loc, Loc] {
-  const [left, top, right, bottom] = wellOrder(p1, p2)
-  return [loc(left, top), loc(right, bottom)]
-}
-
-// Temp functions for compatibility
-/*** BEGIN ***/
-export function toExteriorBox(p1: Loc, p2: Loc): [Loc, Loc] {
-  const [min, max] = wellOrdered(p1, p2)
-  return [min.offset(-1), max.offset(1)]
-}
-
-export function* locIterate(p1: Loc, p2: Loc): IterableIterator<Loc> {
-  const [left, top, right, bottom] = wellOrder(p1, p2)
-
-  for (let y = top; y <= bottom; y++) {
-    for (let x = left; x <= bottom; x++) {
-      yield loc(x, y)
-    }
-  }
-}
-/***  END  ***/
-
-function wellOrder(p1: Loc, p2: Loc): [number, number, number, number] {
-  return [
-    Math.min(p1.x, p2.x),
-    Math.min(p1.y, p2.y),
-    Math.max(p1.x, p2.x),
-    Math.max(p1.y, p2.y),
-  ]
 }
 
 export class Loc {
@@ -102,15 +66,6 @@ export class Loc {
 
   boxR(radius: number) {
     return this.box(radius * 2 + 1)
-  }
-
-  boxCorners(height: number, width?: number): [Loc, Loc] {
-    width ??= height
-    const l = this.x - Math.trunc(width / 2)
-    const t = this.y - Math.trunc(height / 2)
-    const r = l + width - 1
-    const b = t + height - 1
-    return [loc(l, t), loc(r, b)]
   }
 
   isOnEdge(b: Box): boolean {
