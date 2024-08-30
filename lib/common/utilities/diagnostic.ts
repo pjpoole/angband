aa855789d532e46f3a6c32204e4bb7215f438072
Peter Poole <peter.j.poole@gmail.com>
2024-08-30 12:50:01 -0400

    add log limiter diagnostic function

--- lib/common/utilities/diagnostic.ts
+++ lib/common/utilities/diagnostic.ts
@@ -1,8 +1,18 @@
-export function timed(fn: (...args: any) => any, template: string) {
+type WrappedFunction = (...args: any) => any
+
+export function timed(fn: WrappedFunction, template: string) {
   const startTime = Date.now()
   const result = fn()
 
   console.log(template, Date.now() - startTime)
 
   return result
+}
+
+let _times = 100
+
+export function limitLogging(fn: WrappedFunction) {
+  if (_times === 0) return
+  _times--
+  return fn()
 }
