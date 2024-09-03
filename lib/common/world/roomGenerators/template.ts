ad183dffb732045ac4c15f536ece64196bf691fa
Peter Poole <peter.j.poole@gmail.com>
2024-09-03 16:17:29 -0400

    make template room a room generator

--- lib/common/world/roomGenerators/template.ts
+++ lib/common/world/roomGenerators/template.ts
@@ -10,213 +10,213 @@ import { ORIGIN } from '../../objects/origin'
 import { ROOMF, RoomTemplate, RoomTemplateRegistry } from '../roomTemplate'
 import { SQUARE } from '../square'
 
-import {
-  CaveGenerationParams,
-  getCaveParams,
-  getSpaceBox,
-  SizeParams
-} from './helpers'
 import { placeClosedDoor, placeSecretDoor } from './helpers/door'
 import { placeNVaultMonsters } from './helpers/monster'
 import { placeNVaultObjects, placeObject } from './helpers/object'
 import { placeRandomStairs } from './helpers/stairs'
-import {
-  getRandomSymmetryTransform,
-  SymmetryTransform,
-  SYMTR
-} from './helpers/symmetry'
+import { getRandomSymmetryTransform, SYMTR } from './helpers/symmetry'
 import { placeTrap } from './helpers/trap'
+import { RoomGeneratorBase } from './RoomGenerator'
 
 export function build(
   dungeon: Dungeon,
-  chunk: Cave,
+  cave: Cave,
   center: Loc,
   rating: number,
 ): boolean {
   const template = getRandomRoomTemplate(1, rating)
   if (template == null) return false
 
-  return buildRoomTemplate(dungeon, chunk, center, template)
+  const generator = new TemplateRoomGenerator({ template, depth: cave.depth })
+  return generator.draw(dungeon, cave, center)
 }
 
-function getNewCenter(dungeon: Dungeon, chunk: Cave, center: Loc, size: SizeParams): [Loc, SymmetryTransform] | [null, null] {
-  const { height, width } = size
-  let symmetryOp
-  if (!chunk.isInbounds(center)) {
-    symmetryOp = getRandomSymmetryTransform(
-      height,
-      width,
-      SYMTR.FLAG_NONE,
-    )
+export interface TemplateRoomGeneratingParams {
+  template: RoomTemplate,
+  depth: number,
+  rotate?: number,
+  reflect?: boolean,
+}
 
-    const newCenter = dungeon.findSpace(getSpaceBox(center, size))
-    if (newCenter == null) return [null, null]
-    center = newCenter
-  } else {
-    symmetryOp = getRandomSymmetryTransform(
-      height,
-      width,
-      SYMTR.FLAG_NONE,
-      0,
-    )
+export class TemplateRoomGenerator extends RoomGeneratorBase {
+  private rotate: number
+  private reflect: boolean
+  private readonly template: RoomTemplate
 
-    // no rotate
-    assert(symmetryOp.height === height && symmetryOp.width === width)
+  constructor(params: TemplateRoomGeneratingParams) {
+    const { template, depth, rotate, reflect } = params
+    const { height, width } = template
+
+    super({ height, width, depth })
+
+    this.template = template
+    this.reflect = reflect ?? false
+    this.rotate = rotate ? rotate % 4 : 0
   }
 
-  return [center, symmetryOp]
-}
+  protected validateCenter(
+    dungeon: Dungeon,
+    cave: Cave,
+    center: Loc
+  ): Loc | null {
+    const rotationAllowed = cave.isInbounds(center)
+    const allowRotate = rotationAllowed ? undefined : 0
 
-function buildRoomTemplate(
-  dungeon: Dungeon,
-  cave: Cave,
-  center: Loc,
-  template: RoomTemplate,
-): boolean {
-  const size = {
-    height: template.height,
-    width: template.width,
+    const symmetryOp = getRandomSymmetryTransform(
+      this.height,
+      this.width,
+      SYMTR.FLAG_NONE,
+      allowRotate,
+    )
+
+    const { rotate, reflect, height, width } = symmetryOp
+
+    if (!rotationAllowed) {
+      assert(this.height === height && this.width === width)
+    }
+    this.height = height
+    this.width = width
+    this.rotate = rotate
+    this.reflect = reflect
+
+    return super.validateCenter(dungeon, cave, center)
   }
 
-  const [newCenter, symmetryOp] = getNewCenter(dungeon, cave, center, size)
-  if (newCenter == null) return false
-  center = newCenter
+  build(): Cave {
+    const transform = { rotate: this.rotate, reflect: this.reflect }
 
-  const chunk = buildChunk(template, getCaveParams(cave, size), symmetryOp)
+    const chunk = this.getNewCave()
+    const depth = chunk.depth
 
-  cave.composite(chunk, center.box(size.height, size.width))
+    const light = depth <= randInt1(25)
+    // Which random doors will we generate
+    const randomDoor = randInt1(this.template.doors)
+    // Do we generate optional walls
+    const randomWalls = oneIn(2)
 
-  return true
+    this.template.room.transform(transform, (char, p) => {
+      if (char === ' ') return
+
+      const tile = chunk.tiles.get(p)
+      chunk.setFeature(tile, FEAT.FLOOR)
+      assert(tile.isEmpty())
+
+      switch (char) {
+        case '%':
+          chunk.setMarkedGranite(p, SQUARE.WALL_OUTER)
+          if (this.template.flags.has(ROOMF.FEW_ENTRANCES)) {
+            // TODO: append entrance
+          }
+          break
+        case '#':
+          chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
+          break
+        case '+':
+          placeClosedDoor(chunk, p)
+          break
+        case '^':
+          if (oneIn(4)) {
+            placeTrap(chunk, p, -1, depth)
+          }
+          break
+        case 'x':
+          if (randomWalls) chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
+          break
+        case '(':
+          if (randomWalls) placeSecretDoor(chunk, p)
+          break
+        case ')':
+          if (!randomWalls) placeSecretDoor(chunk, p)
+          else chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
+          break
+        case '8':
+          // TODO: or dungeon persist
+          oneIn(5)
+            // TODO: identify if chunk is a quest level
+            ? placeRandomStairs(chunk, p, false)
+            : placeObject(
+              chunk,
+              p,
+              depth,
+              false,
+              false,
+              ORIGIN.SPECIAL,
+              0
+            )
+          break
+        case '9':
+
+          // handled in second pass
+          break
+        case '[':
+          placeObject(chunk, p, depth, false, false, ORIGIN.SPECIAL, 0)
+          break
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+          const doorNum = asInteger(char)
+          doorNum === randomDoor
+            ? placeSecretDoor(chunk, p)
+            : chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
+          break
+      }
+
+      tile.turnOn(SQUARE.ROOM)
+      if (light) tile.turnOn(SQUARE.GLOW)
+    })
+
+    // Second pass to place items after first pass has completed
+    this.template.room.transform(transform, (char, p) => {
+      const tile = chunk.tiles.get(p)
+
+      switch (char) {
+        case '#':
+          assert(tile.isRoom() && tile.isGranite() && tile.has(SQUARE.WALL_SOLID))
+
+          if ((chunk.countNeighbors(p, false, (tile) => tile.isRoom()) === 8)) {
+            tile.turnOff(SQUARE.WALL_SOLID)
+            tile.turnOn(SQUARE.WALL_INNER)
+          }
+          break
+        case '8':
+          assert(tile.isRoom() && (tile.isFloor() || tile.isStair()))
+          placeNVaultMonsters(chunk, p, depth + 2, randInt0(2) + 3)
+          break
+        case '9':
+          const offset1 = loc(2, -2)
+          const offset2 = loc(3, -3)
+
+          assert(tile.isRoom() && tile.isFloor())
+
+          placeNVaultMonsters(
+            chunk,
+            p.diff(offset2),
+            depth + randInt0(2),
+            randInt1(2)
+          )
+          placeNVaultMonsters(
+            chunk,
+            p.diff(offset2),
+            depth + randInt0(2),
+            randInt1(2)
+          )
+
+          if (oneIn(2)) {
+            placeNVaultObjects(chunk, p.diff(offset1), depth, randInt1(2))
+          }
+          if (oneIn(2)) {
+            placeNVaultObjects(chunk, p.diff(offset1), depth, randInt1(2))
+          }
+          break
+      }
+    })
+
+    return chunk
+  }
 }
-
-function buildChunk(
-  template: RoomTemplate,
-  params: CaveGenerationParams,
-  transform: SymmetryTransform,
-): Cave {
-  const chunk = new Cave(params)
-
-  const light = chunk.depth <= randInt1(25)
-  // Which random doors will we generate
-  const randomDoor = randInt1(template.doors)
-  // Do we generate optional walls
-  const randomWalls = oneIn(2)
-
-  template.room.transform(transform, (char, p) => {
-    if (char === ' ') return
-
-    const tile = chunk.tiles.get(p)
-    chunk.setFeature(tile, FEAT.FLOOR)
-    assert(tile.isEmpty())
-
-    switch (char) {
-      case '%':
-        chunk.setMarkedGranite(p, SQUARE.WALL_OUTER)
-        if (template.flags.has(ROOMF.FEW_ENTRANCES)) {
-          // TODO: append entrance
-        }
-        break
-      case '#':
-        chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
-        break
-      case '+':
-        placeClosedDoor(chunk, p)
-        break
-      case '^':
-        if (oneIn(4)) {
-          placeTrap(chunk, p, -1, chunk.depth)
-        }
-        break
-      case 'x':
-        if (randomWalls) chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
-        break
-      case '(':
-        if (randomWalls) placeSecretDoor(chunk, p)
-        break
-      case ')':
-        if (!randomWalls) placeSecretDoor(chunk, p)
-        else chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
-        break
-      case '8':
-        // TODO: or dungeon persist
-        oneIn(5)
-          // TODO: identify if chunk is a quest level
-          ? placeRandomStairs(chunk, p, false)
-          : placeObject(chunk, p, chunk.depth, false, false, ORIGIN.SPECIAL, 0)
-        break
-      case '9':
-
-        // handled in second pass
-        break
-      case '[':
-        placeObject(chunk, p, chunk.depth, false, false, ORIGIN.SPECIAL, 0)
-        break
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-        const doorNum = asInteger(char)
-        doorNum === randomDoor
-          ? placeSecretDoor(chunk, p)
-          : chunk.setMarkedGranite(p, SQUARE.WALL_SOLID)
-        break
-    }
-
-    tile.turnOn(SQUARE.ROOM)
-    if (light) tile.turnOn(SQUARE.GLOW)
-  })
-
-  // Second pass to place items after first pass has completed
-  template.room.transform(transform, (char, p) => {
-    const tile = chunk.tiles.get(p)
-
-    switch (char) {
-      case '#':
-        assert(tile.isRoom() && tile.isGranite() && tile.has(SQUARE.WALL_SOLID))
-
-        if ((chunk.countNeighbors(p, false, (tile) => tile.isRoom()) === 8)) {
-          tile.turnOff(SQUARE.WALL_SOLID)
-          tile.turnOn(SQUARE.WALL_INNER)
-        }
-        break
-      case '8':
-        assert(tile.isRoom() && (tile.isFloor() || tile.isStair()))
-        placeNVaultMonsters(chunk, p, chunk.depth + 2, randInt0(2) + 3)
-        break
-      case '9':
-        const offset1 = loc(2, -2)
-        const offset2 = loc(3, -3)
-
-        assert(tile.isRoom() && tile.isFloor())
-
-        placeNVaultMonsters(
-          chunk,
-          p.diff(offset2),
-          chunk.depth + randInt0(2),
-          randInt1(2)
-        )
-        placeNVaultMonsters(
-          chunk,
-          p.diff(offset2),
-          chunk.depth + randInt0(2),
-          randInt1(2)
-        )
-
-        if (oneIn(2)) {
-          placeNVaultObjects(chunk, p.diff(offset1), chunk.depth, randInt1(2))
-        }
-        if (oneIn(2)) {
-          placeNVaultObjects(chunk, p.diff(offset1), chunk.depth, randInt1(2))
-        }
-        break
-    }
-  })
-
-  return chunk
-}
-
 
 function getRandomRoomTemplate(
   type: number,
